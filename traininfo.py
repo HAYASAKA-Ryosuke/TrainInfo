01ff5555bbbc164b6365c0932fe11b9d27a1cca0
HAYASAKA.Ryosuke <hayasaka.ryosuke@gmail.com>
2012-11-28 02:50:55 +0900

    firstcommit

---
+++ traininfo.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+#-*-coding:utf-8-*-
+import BeautifulSoup
+import urllib2
+import codecs
+import os
+URL='http://www.tetsudo.com/traffic/'
+html=urllib2.urlopen(URL).read()
+soup=BeautifulSoup.BeautifulSoup(html)
+f=codecs.open('Traininfopy.cache','w','utf-8')
+f.write(u'道:')
+f.write(' ')
+f.write(soup.findAll(id='hokai-1')[0].findAll('td')[1].contents[0].contents[0])
+f.write('=')
+f.write(soup.findAll(id='hokai-1')[0].find('img').get('alt'))
+f.write('')
+f.close()
+os.system("cat Traininfopy.cache")
